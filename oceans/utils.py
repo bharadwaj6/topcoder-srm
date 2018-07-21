3e9af570ccf85be7df069123403f57134ac8168f
bharadwaj6 <krishna.bharadwaj6@gmail.com>
2018-07-21 13:37:54 +0530

    added second droplet

--- oceans/utils.py
+++ oceans/utils.py
@@ -6,18 +6,20 @@ from topcoder.settings import DIGITAL_OCEAN_TOKEN
 def get_digital_data(file_object):
     manager = digitalocean.Manager(token=DIGITAL_OCEAN_TOKEN)
     my_droplets = manager.get_all_droplets()
-    droplet = my_droplets[0]
-
     context = {}
-    context['droplet_options'] = {}
-    context['droplet_options']['features'] = droplet.networks
-    context['droplet_options']['memory'] = droplet.memory
-    context['droplet_options']['created_at'] = droplet.created_at
-    context['droplet_options']['region'] = droplet.region
-    context['droplet_options']['size'] = droplet.size
+    context['droplet_details'] = []
 
-    context['total_space'] = droplet.disk
-    context['total_size'] = droplet.size
+    for droplet in my_droplets:
+        details = {}
+        details['features'] = droplet.networks
+        details['memory'] = droplet.memory
+        details['created_at'] = droplet.created_at
+        details['region'] = droplet.region
+        details['size'] = droplet.size
+        details['total_space'] = droplet.disk
+        details['total_size'] = droplet.size
+
+        context['droplet_details'].append(details)
 
     context['file_size'] = file_object.size
     return context
